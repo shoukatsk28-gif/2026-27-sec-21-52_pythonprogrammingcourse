6d1c13237ab4c80d193a823c03ff74313ee0ac73
shoukatsk28-gif <shoukatsk28@gmail.com>
2026-09-04 14:54:48 +0530

    arthmatic operators

---
+++ arthmatic operators exercise.py
@@ -0,0 +1,25 @@
+# Arithmetic Operators
+
+a = 3
+b = 2
+
+# 1. Addition
+print("Addition of a and b:", a + b)
+
+# 2. Subtraction
+print("Subtraction of a and b:", a - b)
+
+# 3. Multiplication
+print("Multiplication of a and b:", a * b)
+
+# 4. Division
+print("Division of a and b:", a / b)
+
+# 5. Modulus (remainder)
+print("Modulus of a and b:", a % b)
+
+# 6. Floor Division
+print("Floor division of a and b:", a // b)
+
+# 7. Exponentiation (power)
+print("Power of a and b:", a ** b)
