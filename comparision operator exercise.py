e22c5ee59258d3eb87aafe3956a3ffbd2d45ffd9
shoukatsk28-gif <shoukatsk28@gmail.com>
2026-09-04 14:35:12 +0530

    comparision operator

---
+++ comparision operator exercise.py
@@ -0,0 +1,9 @@
+#python comparision operartors exercise
+a=50
+b=20
+result=a>b # Greater than comparision operator
+print("result of",a,"b","is:",result)
+
+
+result=a<b # Greater than comparision operator
+print("result of",a,"b" ,"is:",result)
