fdfd2c61d79513b1d79572871fc31af6ca536f20
shoukatsk28-gif <shoukatsk28@gmail.com>
2026-09-07 20:07:01 +0530

    bitwise operators

---
+++ bitwise operators exercise.py
@@ -0,0 +1,21 @@
+#bitwise operators
+a=10
+b=6
+
+#Bitwise AND
+print("a&b=",a&b)
+
+#Bitwise OR
+print("a|b=",a|b)
+
+#Bitwise XOR
+print("a^b=",a^b)
+
+#Bitwise NOT
+print("~a",~a)
+
+#Left Shift
+print("a<<1=",a<<1)
+
+#Right Shift
+print("a>>1=",a>>1)
