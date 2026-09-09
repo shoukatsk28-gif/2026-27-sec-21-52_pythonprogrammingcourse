ee0a8e9807e7382949829add9d0bdc1d6d7e12f1
shoukatsk28-gif <shoukatsk28@gmail.com>
2026-09-09 11:54:57 +0530

    swap two numbers

---
+++ swap two numbers.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value: "))
+b=int(input("Enter the value: "))
+print(f"Before swapping a and b : {a} and {b} ")
+a=a+b
+b=a-b
+a=a-b
+print(f"After swapping a and b : {a} and {b} ")
