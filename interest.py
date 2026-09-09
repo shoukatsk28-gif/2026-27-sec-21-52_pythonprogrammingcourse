41f132c499bd17332f6c8106c10aff09c3d183ee
shoukatsk28-gif <shoukatsk28@gmail.com>
2026-09-09 11:59:26 +0530

    interest

---
+++ interest.py
@@ -0,0 +1,7 @@
+principle = float(input("Enter loan amount: "))
+rate = float(input("Enter rate of interest: "))
+time = float(input("Enter  time period: "))
+
+simple_interest = (principle * rate * time) / 100
+
+print("Simple Interest:", simple_interest)
